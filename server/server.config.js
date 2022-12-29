a34db1f6d8cb56e813d8f0172be333dc5ee3a77f
hdyard <hdyard@gmail.com>
2022-12-29 10:58:56 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: `/server/`,
-    bare: `bare',
+    bare: `/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/server/server.handler.js',
